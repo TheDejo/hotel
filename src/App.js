2b305855afb7570c8a5bc4723cb7e06937b37bca
TheDejo <65681287+TheDejo@users.noreply.github.com>
2021-03-03 14:21:48 +0100

    Adding of Images and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-// import '../App.css'
+import './App.css'
 
 const App = () => {
   return (
---
+++ src/pages/Room.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Room = () => {
+  return (
+    <div>
+      Hello from Rooms page
+    </div>
+  );
+};
+
+export default Room;
